18f1d4ad5d3f40f95b6e68c6c05ccbb371e351b9
Christoph Singer <singer@webagentur72.de>
2020-11-13 18:55:26 +0100

    Refactor and fix mapping and settings comparison

--- src/functions.php
+++ src/functions.php
@@ -36,33 +36,61 @@ function array_diff_assoc_recursive($array1, $array2)
 }
 
 /**
- * Compute difference between two multidimensional associative arrays
+ * Compute difference between two multidimensional associative arrays.
  *
  * @param array $array1
  * @param array $array2
  * @return array Associative array:
  *                  Key "-" contains elements from $array1 that have no match in $array2,
  *                  Key "+" contains elements from $array2 that have no match in $array1,
+ *                  element key paths are given in dot notation (a.b.c instead of [a][b][c]);
  *                  empty array if both input arrays are equal
  */
 function compare_assoc_arrays($array1, $array2)
 {
-    if (empty($array1) && empty($array2)) {
+    if ($array1 == $array2) {
         return [];
     } elseif (empty($array1) && !empty($array2)) {
         return ['+' => $array2];
     } elseif (!empty($array1) && empty($array2)) {
         return ['-' => $array1];
+    } elseif ((!is_array($array1) || !is_array($array2)) &&  $array1 != $array2) {
+        return ['-' => $array1, '+' => $array2];
     } else {
         $d = [];
-        $d1 = array_diff_assoc_recursive($array1, $array2);
-        if (!empty($d1)) {
-            $d['-'] = $d1;
-        }
-        $d2 = array_diff_assoc_recursive($array2, $array1);
-        if (!empty($d2)) {
-            $d['+'] = $d2;
-        }
+
+        $compare = function($a1, $a2, string $path, &$r) use (&$compare) {
+            if ($a1 == $a2) {
+                return;
+            } elseif (empty($a1) && !empty($a2)) {
+                $r['+'][$path] = $a2;
+                return;
+            } elseif (!empty($a1) && empty($a2)) {
+                $r['-'][$path] = $a1;
+                return;
+            } elseif ((!is_array($a1) || !is_array($a2)) &&  $a1 != $a2) {
+                $r['-'][$path] = $a1;
+                $r['+'][$path] = $a2;
+                return;
+            } else {
+                foreach ($a1 as $k1 => $v1) {
+                    $subpath = $path . ($path ? '.' : '') . $k1;
+                    if (isset($a2[$k1])) {
+                        $compare($v1, $a2[$k1], $subpath, $r);
+                    } else {
+                        $r['-'][$subpath] = $v1;
+                    }
+                }
+                foreach ($a2 as $k2 => $v2) {
+                    $subpath = $path . ($path ? '.' : '') . $k2;
+                    if (!isset($a1[$k2])) {
+                        $r['+'][$subpath] = $v2;
+                    }
+                }
+            }
+        };
+        $compare($array1, $array2, '', $d);
+
         return $d;
     }
 }
